16d4947c185baa44c97ba9013474e47e7eeb8929
Jereneal <jereneal20@gmail.com>
2016-04-03 23:00:23 +0900

    Refactor FormulateCNF

---
+++ nonogram/FormulateCNF.py
@@ -0,0 +1,94 @@
+import os, sys
+from collections import deque
+sys.path.insert(0, '../../cnf/cnf')
+
+import ConvertToCNF as CNF
+from Formula import Formula
+
+
+def createCNF(input, enumerated):
+	rowEnumerate = enumerated[0]
+	columnEnumerate = enumerated[1]
+
+	formulaDic = getDNFFormulaDic(input, rowEnumerate)
+	formulaDic2 = getDNFFormulaDic(input, columnEnumerate)
+
+	for iter in formulaDic2:
+		formulaDic.append(iter)
+
+	print(formulaDic)
+
+	return getCNFformEntireLine(formulaDic)
+
+
+def getDNFFormulaDic(input, enumerates):
+	oldVar = 0
+	formulaDic = []
+	formulaDic.append([])
+	for iter in enumerates:
+		dnfInput = formulateOneCase(input, iter)
+		dnfForm = Formula(deque(dnfInput.split()))
+		if iter[1] != oldVar:
+			oldVar = iter[1]
+			formulaDic.append([])
+		formulaDic[oldVar].append(dnfForm)
+	return formulaDic
+
+
+def formulateOneCase(input, locArray):
+
+	dnfStr = ""
+	numOfVar = 0
+	if locArray[0] == 1:
+		numOfVar = input[2]
+	else:
+		numOfVar = input[0]
+
+	for iter in range(numOfVar-1):
+		dnfStr += "& "
+	lst = [0 for _ in range(numOfVar)]
+
+	for j, k in zip(locArray[2], locArray[3]):
+		for iter in range(numOfVar):
+			if k <= iter and iter < j + k:
+				lst[iter] = 1
+
+	iterNum = 0
+	for isNot in lst:
+		if locArray[0] == 1: # row
+			if isNot == 1:
+				dnfStr += "a"+str(locArray[1])+str(iterNum) +" "
+			else:
+				dnfStr += "- a" + str(locArray[1]) + str(iterNum) + " "
+		else:
+			if isNot == 1:
+				dnfStr += "a" + str(iterNum)+ str(locArray[1]) +" "
+			else:
+				dnfStr += "- a" + str(iterNum) + str(locArray[1]) + " "
+		iterNum += 1
+
+	# print(dnfStr)
+	return dnfStr
+
+
+
+
+def getCNFformOneLine(formulaList):
+	masterFormula = formulaList[0]
+	for iter in formulaList:
+		if iter != masterFormula:
+			masterFormula = CNF.wrapBinFormula("|", masterFormula, iter)
+	return CNF.conjunctiveNormalForm(masterFormula)
+
+def getCNFformEntireLine(formulaDic):
+	cnfFormList = []
+	for iter in formulaDic:
+		oneLineFormula = getCNFformOneLine(iter)
+		cnfFormList.append(oneLineFormula)
+	print(cnfFormList)
+
+	masterFormula = cnfFormList[0]
+	for iter in cnfFormList:
+		if iter != masterFormula:
+			masterFormula = CNF.wrapBinFormula("&", masterFormula, iter)
+	return masterFormula
--- nonogram/__init__.py
+++ nonogram/__init__.py
@@ -7,13 +7,13 @@ from Formula import Formula
 import ConvertToCNF as CNF
 import MiniSATCaller as minisat
 import CaseEnumerator
+import FormulateCNF
 
 sampleInput2 = "> & - p q & p > r q"
 sampleInput = [6, [[2, 1],[ 1, 3], [1, 2], [3], [4], [1]],
 			   6, [[1], [5], [2], [5], [2, 1], [2]]]
-sampleInput3 = [2, [[2, 1, 2],[ 1, 3] ],
+sampleInput3 = [2, [[2, 1, 2],[1, 3]],
 			   10, [[1], [5], [2], [5], [2, 1], [2],[2], [5], [2, 1], [2]]]
-
 
 
 def getInputFromUser():
@@ -35,75 +35,15 @@ def getInputFromUser():
 
 	return [numOfRows,rowInfo,numOfCols,columnInfo]
 
-def formulateOneCase(input, locArray):
-
-	dnfStr = ""
-	numOfVar = 0
-	if locArray[0] == 1:
-		numOfVar = input[2]
-	else:
-		numOfVar = input[0]
-
-
-	for iter in range(numOfVar-1):
-		dnfStr += "& "
-	lst = [0 for _ in range(numOfVar)]
-
-	# for locIter in range(len(locArray[2])):
-	# 	j = locArray[2][locIter]
-	# 	k = locArray[3][locIter]
-	for j, k in zip(locArray[2], locArray[3]):
-		for iter in range(numOfVar):
-			if k <= iter and iter < j + k:
-				lst[iter] = 1
-	# print(lst)
-	iterNum = 0
-	for isNot in lst:
-		if locArray[0] == 1: # row
-			if isNot == 1:
-				dnfStr += "a"+str(locArray[1])+str(iterNum) +" "
-			else:
-				dnfStr += "- a" + str(locArray[1]) + str(iterNum) + " "
-		else:
-			if isNot == 1:
-				dnfStr += "a" + str(iterNum)+ str(locArray[1]) +" "
-			else:
-				dnfStr += "- a" + str(iterNum) + str(locArray[1]) + " "
-		iterNum += 1
-
-	# print(dnfStr)
-	return dnfStr
-
-def getDNFFormulaDic(enumerates):
-	oldVar = 0
-	formulaDic = []
-	formulaDic.append([])
-	for iter in enumerates:
-		dnfInput = formulateOneCase(sampleInput, iter)
-		dnfForm = Formula(deque(dnfInput.split()))
-		if iter[1] != oldVar:
-			oldVar = iter[1]
-			formulaDic.append([])
-		formulaDic[oldVar].append(dnfForm)
-	return formulaDic
 
 def main(argv):
 	# print(getInputFromUser())
 
-	(rowEnumerate, columnEnumerate) = CaseEnumerator.caseEnumaration(sampleInput)
-	print(rowEnumerate)
-	print(columnEnumerate)
+	enumerated = CaseEnumerator.caseEnumaration(sampleInput)
+	print(enumerated[0])
+	print(enumerated[1])
 
-	formulaDic = getDNFFormulaDic(rowEnumerate)
-	formulaDic2 = getDNFFormulaDic(columnEnumerate)
-
-	for iter in formulaDic2:
-		formulaDic.append(iter)
-
-	print(formulaDic)
-
-
-	resultFormula = getCNFformEntireLine(formulaDic)
+	resultFormula = FormulateCNF.createCNF(sampleInput, enumerated)
 
 
 	infix = resultFormula.formulaAsInfixString()
@@ -111,28 +51,6 @@ def main(argv):
 	miniSATStr = minisat.getMiniSATString(infix, varDictionary)
 	print(minisat.getMiniSATResult(miniSATStr))
 
-	originalFormula = Formula(deque(sampleInput2.split()))
-	print(originalFormula.formulaAsString())
-
-def getCNFformOneLine(formulaList):
-	masterFormula = formulaList[0]
-	for iter in formulaList:
-		if iter != masterFormula:
-			masterFormula = CNF.wrapBinFormula("|", masterFormula, iter)
-	return CNF.conjunctiveNormalForm(masterFormula)
-
-def getCNFformEntireLine(formulaDic):
-	cnfFormList = []
-	for iter in formulaDic:
-		oneLineFormula = getCNFformOneLine(iter)
-		cnfFormList.append(oneLineFormula)
-	print(cnfFormList)
-
-	masterFormula = cnfFormList[0]
-	for iter in cnfFormList:
-		if iter != masterFormula:
-			masterFormula = CNF.wrapBinFormula("&", masterFormula, iter)
-	return masterFormula
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
